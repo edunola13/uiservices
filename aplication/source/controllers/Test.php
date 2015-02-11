3654649c6474c34e0a63dd5ee40df13ca484c401
edunola13 <edunola13@hotmail.com>
2015-02-11 12:10:48 -0300

    Mejora y nuevos componentes

--- aplication/source/controllers/Test.php
+++ aplication/source/controllers/Test.php
@@ -19,7 +19,8 @@ class Test extends En_Controller{
      * Devuelve la definicion de un componente 
      */
     public function doGet(){
-        $this->config= array('label' => 'Formulario');
+        $this->config2= array('state' => 'active','src' => 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDkwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzc3NyIvPjxnPjx0ZXh0IHg9IjMxNy43MzQzNzUiIHk9IjI1MCIgc3R5bGU9ImZpbGw6IzU1NTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZTo0MnB0O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPkZpcnN0IHNsaWRlPC90ZXh0PjwvZz48L3N2Zz4=');
+        $this->config= array('labelPrevious' => 'Formulario', 'href' => '#', 'labelNext' => 'Next');
         $this->load_view('test');
     }
 }
--- aplication/source/view/test.php
+++ aplication/source/view/test.php
@@ -5,12 +5,17 @@
     <title>Pruebas</title>  
     <link href="<?php echo BASEURL . 'resources/css/bootstrap.min.css'; ?>" rel="stylesheet">
     <link href="<?php echo BASEURL . 'resources/css/bootstrap-theme.min.css'; ?>" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php echo BASEURL . 'resources/js/bootstrap.min.js' ;?>"></script>
 </head>
 <body>    
     <div class="container">
-        <?php echo $this->twig->render("componentes/bootstrap3/formulario/formulario.html.twig", array("hijos" => $this->hijos, "config" => $this->config, "datos" => $this->datos));?>
-    </div>
-      
+        <?php $var= $this->twig->render("componentes/bootstrap3/otros/carousel_item.html.twig", array("hijos" => $this->hijos, "config" => $this->config2, "datos" => $this->datos));?>
+        <?php $var2= $this->twig->render("componentes/bootstrap3/otros/carousel_item.html.twig", array("hijos" => $this->hijos, "config" => $this->config2, "datos" => $this->datos, 'state'=>'active'));?>
+        <?php echo $this->twig->render("componentes/bootstrap3/otros/carousel.html.twig", array("hijos" => $var . $var2, "config" => $this->config, "datos" => $this->datos));?>
+    </div>    
+        
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="<?php echo BASEURL . 'resources/js/bootstrap.min.js' ?>"></script>
 </body>
